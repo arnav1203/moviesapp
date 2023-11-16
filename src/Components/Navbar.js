807010800f4c300282c47c33744741eddef6a1d3
arnav1203 <2101641530040@psit.ac.in>
2023-11-17 01:50:53 +0530

    Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,11 +10,11 @@ export default function Navbar() {
                         Website Logo
                     </div>
                 </div>
-                <div className='flex w-2/3 justify-around items-center'>
+                <div className='flex w-2/3 justify-around items-center h-7'>
 
                     <div className='flex justify-end'>
                         <label className='flex'>
-                            <input className='flex rounded-lg placeholder:p-2 p-2 bg-gray-200' placeholder='Search' />
+                            <input className='flex rounded-lg placeholder:p-2 p-1 bg-gray-200' placeholder='Search' />
                             <button><img src={logo} className='mx-2' /></button>
                         </label>
                     </div>
