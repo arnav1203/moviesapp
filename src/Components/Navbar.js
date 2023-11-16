6582cab92aafe7c1a45bdb0c0041207cc8534c32
arnav1203 <2101641530040@psit.ac.in>
2023-11-16 17:42:07 +0530

    Navbar done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,30 +2,33 @@ import React, { useState } from 'react'
 import logo from './assets/search.svg'
 
 export default function Navbar() {
-    const [sc, usc] = useState(false);
-    const handlehover = () => {
-        usc(true);
-    }
-    const leavehover = () => {
-        usc(false);
-    }
     return (
         <div>
-            <div className='flex justify-between p-5'>
+            <div className='flex justify-between items-center p-5'>
                 <div>
                     <div className='text-xl font-semibold text-white'>
                         Website Logo
                     </div>
                 </div>
-                <div className='flex w-2/3 justify-evenly'>
+                <div className='flex w-2/3 justify-around items-center'>
 
-                    <div className=''>
-                        <label className='flex' onMouseEnter={handlehover} onMouseLeave={leavehover}>
-                            <input className={`${sc ? 'flex' : 'hidden'} mx-2`} />
-                            <img src={logo} />
+                    <div className='flex justify-end'>
+                        <label className='flex'>
+                            <input className='flex rounded-lg placeholder:p-2 p-2 bg-gray-200' placeholder='Search' />
+                            <button><img src={logo} className='mx-2' /></button>
                         </label>
                     </div>
-                    <div className='text-lg font-semibold relative'>
+                    <div className='text-lg font-semibold '>
+                        <button className='bg-clip-text text-transparent bg-white hover:scale-110'>
+                            HOME
+                        </button>
+                    </div>
+                    <div className='text-lg font-semibold '>
+                        <button className='bg-clip-text text-transparent bg-white hover:scale-110'>
+                            ABOUT
+                        </button>
+                    </div>
+                    <div className='text-lg font-semibold '>
                         <button className='bg-clip-text text-transparent bg-white hover:scale-110'>
                             LOGIN
                         </button>
