030593208ac43307bbfdcec14906f1575ca2baee
arnav1203 <2101641530040@psit.ac.in>
2023-11-22 16:28:15 +0530

    Details page structure

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -3,7 +3,26 @@ import React from 'react'
 export default function Details(props) {
     return (
         <div>
-            Hello
+            <div className='flex flex-nowrap overflow-x-auto no-scrollbar mr-5'>
+                {props.movies.map((movie, index) =>
+                    <div className='flex justify-around'>
+                        <div>
+                            <img src={props.movie.poster_path} alt='poster' />
+                        </div>
+                        <div>
+                            <div className='text-3xl'>
+                                {props.movie.title}
+                            </div>
+                            <div>
+                                {props.movie.overview}
+                            </div>
+                            <div>
+                                {props.movie.release_date}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
