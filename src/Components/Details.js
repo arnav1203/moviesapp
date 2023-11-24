d497019cf1e7d1fef98c5561017e484cb85ba4ed
arnav1203 <2101641530040@psit.ac.in>
2023-11-24 15:57:55 +0530

    Personal movie details fetched

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,18 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 
 export default function Details(item) {
+    const [select, setselect] = useState('');
+    const [detail, setdetail] = useState([]);
     useEffect(() => {
         console.log(item)
-    })
+        // console.log(item.movies._id)
+        setselect(item.movies._id)
+        const apikey = process.env.REACT_APP_XRapidAPIKey;
+
+        const fetchMovies = async () => {
+            const url = `https://movies-api14.p.rapidapi.com/movie/${select}`;
+
+            const headers = {
+                'X-RapidAPI-Host': 'movies-api14.p.rapidapi.com',
+                'X-RapidAPI-Key': apikey,
+                'Content-Type': 'application/json',
+            };
+
+
+            try {
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: headers,
+                });
+
+
+                const data = await response.json();
+                // console.log(data.movie._id);
+                setdetail(data);
+                // console.log(trending.movies);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchMovies();
+        console.log(detail.movie._id);
+    }, [item])
     return (
         <div className=''>
             <Navbar />
             <div className='no-scrollbar mr-5'>
                 {/* {props.movies.map((movie, index) => */}
-                <div className=' mt-24 ml-24 '>
-                    <div className='h-96 w-full flex justify-center p-3'>
-                        <img src={item.movies.backdrop_path} className='h-full w-3/4 object-contain' alt='movie_img' />
+                <div className=' mt-10 ml-24 '>
+                    <div className='h-full w-full flex justify-center p-3'>
+                        <img src={item.movies.backdrop_path} className='h-full w-2/3 object-contain' alt='movie_img' />
                     </div>
                     <div className='space-y-5 w-1/3'>
                         <div className='text-3xl text-white underline'>
@@ -23,6 +56,9 @@ export default function Details(item) {
                         </div>
                         <div className=' text-white'>
                             Release Date: {item.movies.release_date}
+                        </div>
+                        <div>
+                            {detail.movie.youtube_trailer}
                         </div>
                     </div>
                 </div>
