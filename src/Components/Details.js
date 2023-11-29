92d680d88a67ca9c457fd8985eede6f808f6efe5
arnav1203 <2101641530040@psit.ac.in>
2023-11-30 00:24:57 +0530

    Anchor tag to trailer div

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -71,7 +71,7 @@ export default function Details(item) {
                                 {item.movies.overview}
                             </div>
                             <div className='xl:flex hidden'>
-                                <a href="#_" class="bg-slate-800 relative inline-flex items-center justify-center p-3 px-4 py-2 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-900 rounded-full shadow-md group">
+                                <a href="#trailer" class="bg-slate-800 relative inline-flex items-center justify-center p-3 px-4 py-2 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-900 rounded-full shadow-md group">
                                     <span class="absolute inset-0 flex items-center justify-center w-full h-full text-gray-300 duration-300 -translate-x-full bg-indigo-900 group-hover:translate-x-0 ease">
                                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                     </span>
@@ -82,7 +82,7 @@ export default function Details(item) {
                         </div>
                     </div>
                     {detail && detail.movie && (
-                        <div className='lg:h-[800px] h-96 py-8'>
+                        <div className='lg:h-[800px] h-96 py-8' id='trailer'>
                             <ReactPlayer url={detail.movie.youtube_trailer} width='100%' height='100%' />
                         </div>
                     )}
