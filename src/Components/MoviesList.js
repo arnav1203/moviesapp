c1ac779c92169ba1e57a70d4eb4d1e180eecbad0
arnav1203 <2101641530040@psit.ac.in>
2023-12-01 00:38:00 +0530

    Title width set

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -23,7 +23,7 @@ export default function MoviesList(props) {
                         <div className='h-72 w-44 hover:scale-110 p-3'>
                             <img src={movie.poster_path} className='h-full w-full object-cover' alt='movie_img'></img>
                         </div>
-                        <div className='space-y-2'>
+                        <div className='space-y-2 w-44'>
                             <div className='text-white'>
                                 {movie.title}
                             </div>
