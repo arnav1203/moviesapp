779452fa73e22c9aab0431b0d7e5f12f520c05b5
arnav1203 <2101641530040@psit.ac.in>
2023-12-05 14:21:34 +0530

    details update

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -56,7 +56,7 @@ export default function Details(props) {
                         <div className='xl:absolute xl:inset-8  space-y-5 xl:w-1/3'>
                             <div className='xl:pt-40'>
                                 <div className='flex pb-7'>
-                                    <p className='text-4xl text-white'>{props.movies.title}</p>
+                                    <p className='text-4xl text-white font-medium'>{props.movies.title}</p>
 
                                 </div>
                                 {detail && detail.movie && (
@@ -73,12 +73,12 @@ export default function Details(props) {
                                 {props.movies.overview}
                             </div>
                             <div className='xl:flex hidden'>
-                                <a href="#trailer" class="bg-slate-800 relative inline-flex items-center justify-center p-3 px-4 py-2 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-900 rounded-full shadow-md group">
-                                    <span class="absolute inset-0 flex items-center justify-center w-full h-full text-gray-300 duration-300 -translate-x-full bg-indigo-900 group-hover:translate-x-0 ease">
-                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                <a href="#trailer" className="bg-slate-800 relative inline-flex items-center justify-center p-3 px-4 py-2 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-900 rounded-full shadow-md group">
+                                    <span className="absolute inset-0 flex items-center justify-center w-full h-full text-gray-300 duration-300 -translate-x-full bg-indigo-900 group-hover:translate-x-0 ease">
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                     </span>
-                                    <span class="absolute flex items-center justify-center w-full h-full text-gray-300 transition-all duration-300 transform group-hover:translate-x-full ease">Watch Trailer</span>
-                                    <span class="relative invisible">Watch Trailer</span>
+                                    <span className="absolute flex items-center justify-center w-full h-full text-gray-300 transition-all duration-300 transform group-hover:translate-x-full ease">Watch Trailer</span>
+                                    <span className="relative invisible">Watch Trailer</span>
                                 </a>
                             </div>
                         </div>
