9399a6b328841c576155ea5bb96d552766b7922a
arnav1203 <2101641530040@psit.ac.in>
2023-12-06 17:33:12 +0530

    Heading

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 export default function About() {
     return (
         <div>
-
+            <Navbar />
+            <div>
+                <div className='text-3xl text-white'>
+                    About
+                </div>
+            </div>
         </div>
     )
 }
