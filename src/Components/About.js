09dd02effd79fbd26edba17e5a0e319a252a4c45
arnav1203 <2101641530040@psit.ac.in>
2023-12-06 17:40:46 +0530

    About part

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,9 @@ export default function About() {
                     About
                 </div>
                 <div>
-
+                    <div className='text-2xl text-white'>
+                        Welcome to CineScopeHub, the ultimate movie hub designed for every cinephile! Our platform is a passionate endeavor to bring together the world of cinema in one seamless space. Discover, explore, and immerse yourself in a treasure trove of movies, from trending blockbusters to new releases and timeless animated wonders. Whether you seek movie details, trailers, or simply want to indulge your cinematic curiosity, CineScopeHub is your go-to destination. Join us on this cinematic journey where every movie lover finds their story!
+                    </div>
                 </div>
             </div>
         </div>
