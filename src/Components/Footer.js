d3e88ca7dd409b27c9a879bd1b709b7227013332
arnav1203 <2101641530040@psit.ac.in>
2024-02-02 00:14:39 +0530

    Update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import logo from './assets/movieslogo.png'
 export default function Footer() {
     return (
-        <div className='bg-slate-700 px-16 py-10'>
+        <div className='bg-slate-700 px-16 pb-4'>
             <div className='flex flex-wrap justify-around'>
                 <div className='w-1/2 flex justify-around'>
                     <div>
                         <img src={logo} className='h-36 w-36' />
                     </div>
-                    <div className=' xl:w-1/2'>
+                    <div className=' xl:w-2/3'>
                         <div className='py-3'>
-                            <p className='text-gray-400 text-4xl'>About</p>
+                            <p className='text-gray-400 text-4xl'>About Us</p>
                         </div>
                         <div className='text-sm text-white space-y-4'>
                             <p>1HD is free tv shows streaming website with zero ads, it allows you watch tv shows online, watch tv shows online free in high quality for free. You can also download full tv shows and watch it later if you want.
