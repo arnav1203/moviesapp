9228daebe7e42ae8767450205758c6309023b5dd
arnav1203 <2101641530040@psit.ac.in>
2024-02-02 15:48:10 +0530

    Form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,19 +2,28 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div className='flex justify-center flex-col'>
+        <div className='flex flex-col w-1/2'>
             <div>
                 <h1 className='text-4xl text-white'>Contact Us</h1>
             </div>
             <div>
                 <p className='text-white'>Please submit your inquiry using the form below and we will get in touch with you shortly.</p>
             </div>
-            <div>
+            <div className='flex flex-col justify-center'>
                 <form>
-                    <input placeholder='Your email' />
-                    <input placeholder='Subject' />
-                    <p>Message</p>
-                    <input placeholder='Type Here' />
+                    <div className='p-3 sm:w-full pb-3'>
+                        <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Your email" ></textarea>
+
+                    </div>
+                    <div className='p-3 sm:w-full pb-3'>
+                        <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Subject" ></textarea>
+
+                    </div>
+                    <div className='p-3 sm:w-full pb-3'>
+                        <label className='text-gray-400 text-xl p-2'>Message:</label>
+                        <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Type Here" ></textarea>
+
+                    </div>
                 </form>
             </div>
         </div>
