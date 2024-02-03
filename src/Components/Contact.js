0253eb3c2781bd86eb5fb6f0f8b21430d90cdb2b
arnav1203 <2101641530040@psit.ac.in>
2024-02-04 00:29:13 +0530

    Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -20,7 +20,7 @@ export default function Contact() {
                             <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Subject" ></textarea>
 
                         </div>
-                        <div className='p-3 sm:w-full pb-3'>
+                        <div className='p-3 sm:w-full'>
                             <label className='text-gray-400 text-xl p-2'>Message:</label>
                             <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Type Here" ></textarea>
 
