a24f7c40673a6187f9ea5ed11b0770a905de6a5b
arnav1203 <2101641530040@psit.ac.in>
2024-02-13 12:05:18 +0530

    Data updating successfully

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,8 +7,8 @@ export default function Contact() {
         message: ''
     })
     const handlechange = (e) => {
-        setdata = ({
-            ...data, [e.target.email]: e.target.value
+        setdata({
+            ...data, [e.target.name]: e.target.value
         })
     }
     const handlesubmit = () => {
@@ -28,26 +28,26 @@ export default function Contact() {
                     <div className='flex flex-col justify-center'>
                         <form className='space-y-7' onSubmit={handlesubmit}>
                             <div className=' sm:w-full '>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-1.5" placeholder="Your email" onChange={handlechange} ></textarea>
+                                <textarea type="text" name='email' className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-1.5" placeholder="Your email" onChange={handlechange} ></textarea>
 
                             </div>
                             <div className=' sm:w-full '>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Subject" onChange={handlechange} ></textarea>
+                                <textarea type="text" name='subject' className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Subject" onChange={handlechange} ></textarea>
 
                             </div>
                             <div className=' sm:w-full space-y-4'>
                                 <label className='text-white text-xl p-2'>Message:</label>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Type Here" onChange={handlechange} ></textarea>
+                                <textarea type="text" name='message' className="bg-zinc-700 resize-y border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Type Here" onChange={handlechange} ></textarea>
 
                             </div>
                             <div>
-                                <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit'>
-                                    <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
-                                    <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
-                                    <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
-                                    <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-                                    <span class="relative">Submit</span>
-                                </a>
+                                <button className="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit'>
+                                    <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                                    <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                                    <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                                    <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                                    <span className="relative">Submit</span>
+                                </button>
                             </div>
                         </form>
                     </div>
