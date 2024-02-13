d3d072185bb88bf8d0482f2cfc602c39edbb2858
arnav1203 <2101641530040@psit.ac.in>
2024-02-13 11:53:51 +0530

    Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 export default function Contact() {
-    const submitb = () => {
-
+    const [data, setdata] = useState({
+        email: '',
+        subject: '',
+        message: ''
+    })
+    const handlechange = (e) => {
+        setdata = ({
+            ...data, [e.target.email]: e.target.value
+        })
+    }
+    const handlesubmit = () => {
+        console.log(data);
     }
     return (
         <div>
@@ -16,22 +26,22 @@ export default function Contact() {
                         <p className='text-white'>Please submit your enquiry using the form below and we will get in touch with you shortly.</p>
                     </div>
                     <div className='flex flex-col justify-center'>
-                        <form className='space-y-7'>
+                        <form className='space-y-7' onSubmit={handlesubmit}>
                             <div className=' sm:w-full '>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Your email" ></textarea>
+                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-1.5" placeholder="Your email" onChange={handlechange} ></textarea>
 
                             </div>
                             <div className=' sm:w-full '>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Subject" ></textarea>
+                                <textarea type="text" id="first_name" className="bg-zinc-700 border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Subject" onChange={handlechange} ></textarea>
 
                             </div>
                             <div className=' sm:w-full space-y-4'>
                                 <label className='text-white text-xl p-2'>Message:</label>
-                                <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Type Here" ></textarea>
+                                <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-200 rounded-lg block w-full p-2.5" placeholder="Type Here" onChange={handlechange} ></textarea>
 
                             </div>
                             <div>
-                                <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit' onClick={submitb()}>
+                                <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit'>
                                     <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
                                     <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
                                     <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
