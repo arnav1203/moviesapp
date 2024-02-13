642c769ce40ce538d4f42ff51a07085811fa1519
arnav1203 <2101641530040@psit.ac.in>
2024-02-13 11:26:04 +0530

    Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 export default function Contact() {
+    const submitb = () => {
+
+    }
     return (
         <div>
             <Navbar />
@@ -28,7 +31,7 @@ export default function Contact() {
 
                             </div>
                             <div>
-                                <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit'>
+                                <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block" type='submit' onClick={submitb()}>
                                     <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
                                     <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
                                     <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
