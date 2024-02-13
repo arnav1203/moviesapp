a9765bf1e56c9db9cbfb4cc7cafb856485bc0016
arnav1203 <2101641530040@psit.ac.in>
2024-02-13 11:08:53 +0530

    Button

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -25,6 +25,15 @@ export default function Contact() {
                             <textarea type="text" id="first_name" className="bg-zinc-700 resize-y border border-gray-300 text-gray-300 rounded-lg block w-full p-2.5" placeholder="Type Here" ></textarea>
 
                         </div>
+                        <div>
+                            <a href="#_" class="px-5 py-2.5 relative rounded group text-white font-medium inline-block">
+                                <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                                <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                                <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                                <span class="relative">Button Text</span>
+                            </a>
+                        </div>
                     </form>
                 </div>
             </div>
