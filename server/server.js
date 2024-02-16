b61b74a5b881586ead02d6f67163167e375506a5
arnav1203 <2101641530040@psit.ac.in>
2024-02-16 12:16:23 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const nodemailer = require('nodemailer')
 const bodyparser = require('body-parser')
 const app = express()
 app.use(bodyparser.json())
@@ -7,6 +8,7 @@ app.use(cors())
 
 app.post('/submitform', (req, res) => {
     const { name, email, message } = req.body;
+
 
 })
 
