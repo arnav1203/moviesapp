bb48f0cecda6ef897c10407a9decdebaac53429d
arnav1203 <2101641530040@psit.ac.in>
2024-02-22 17:54:11 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+mongoose.connect('mongodb://127.0.0.1:27017')
+
 app.post('/submitform', (req, res) => {
     const { name, email, message } = req.body;
 
