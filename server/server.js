0f588790e5f2d14aa8901da91b032b9d5754d098
arnav1203 <2101641530040@psit.ac.in>
2024-02-22 18:01:21 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1:27017/Employees')
+
+
 
 app.post('/submitform', (req, res) => {
     const { name, email, message } = req.body;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -34,7 +34,7 @@ export default function Signup() {
                             </div>
                         </button>
                     </div>
-                    <div className='flex justify-center p-3'><span>Already a member?</span><a href='/Login' className='text-indigo-800 hover:underline'>Login Here</a></div>
+                    <div className='flex justify-center p-3'><span>Already a member?</span><a href='/Login' className='text-indigo-800 hover:underline' type='submit'>Login Here</a></div>
 
                 </form>
             </div>
