26e0b1f8deb934e460a94c36a13dfc73115bddc3
arnav1203 <2101641530040@psit.ac.in>
2024-04-30 17:30:23 +0530

    Update

--- client/src/Components/SearchList.js
+++ client/src/Components/SearchList.js
@@ -7,7 +7,7 @@ export default function SearchList({ results }) {
         //         <h1>{result.title}</h1>
         //     </div>
         // })
-        <div className='text-white p-2 bg-blue-300'>
+        <div className='text-white px-2 pt-1 bg-blue-300'>
             <h1 className='border-b-2 text-xl h-10'>SearchList</h1>
             <h1 className='border-b-2 text-xl h-10'>SearchList</h1>
             <h1 className='border-b-2 text-xl h-10'>SearchList</h1>
